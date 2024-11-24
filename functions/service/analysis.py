7984c47fc8063e668ed17768bdd85824a9a2ac19
light-src <rhkddnjs99@hotmail.com>
2024-11-24 23:11:52 +0900

    feat: analaysis 업데이트 시 annual, quarter 값 모두 갖도록

--- functions/service/analysis.py
+++ functions/service/analysis.py
@@ -1,4 +1,10 @@
 from service.firestore import FirestoreService
+from datetime import datetime
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class AnalysisService:
@@ -6,11 +12,51 @@ class AnalysisService:
         self.firestore = FirestoreService()
 
     def update_analysis(self, symbol, quote):
-        balancesheet = self.firestore.get_balancesheet(symbol, "2024", "FY")
+        annual_ncav_ratio = self.get_annual_ncav(symbol, quote)
+        quarter_ncav_ratio = self.get_quarter_ncav(symbol, quote)
 
-        # Calculate NCAV
+        self.firestore.store_analysis(symbol, {
+            "annual_ncav_ratio": annual_ncav_ratio,
+            "quarter_ncav_ratio": quarter_ncav_ratio
+        })
+
+    def get_annual_ncav(self, symbol, quote) -> float:
+        current_year = datetime.now().year
+
+        for year in [current_year, current_year - 1]:  # 현재년도 → 작년도 순서로 시도
+            balancesheet = self.firestore.get_balancesheet(symbol, str(year), "FY")
+            if balancesheet:  # 데이터가 있으면 중단
+                break
+
+        if not balancesheet:
+            logger.warning(f"Balancesheet not found for {symbol}")
+            return -1
+
         nca = balancesheet.get("totalCurrentAssets") - balancesheet.get("totalLiabilities")
         market_cap = quote["marketCap"]
         ncav_ratio = nca / market_cap
 
-        self.firestore.store_analysis(symbol, {"ncav": ncav_ratio})
+        return ncav_ratio
+
+    def get_quarter_ncav(self, symbol, quote) -> float:
+        current_year = datetime.now().year
+        balancesheet = None
+
+        quarters = [
+            (current_year, "Q4"), (current_year, "Q3"), (current_year, "Q2"), (current_year, "Q1"),
+            (current_year - 1, "Q4"), (current_year - 1, "Q3")
+        ]
+        for year, quarter in quarters:
+            balancesheet = self.firestore.get_balancesheet(symbol, str(year), quarter)
+            if balancesheet:
+                break
+
+        if not balancesheet:
+            logger.warning(f"Balancesheet not found for {symbol}")
+            return -1
+
+        nca = balancesheet.get("totalCurrentAssets") - balancesheet.get("totalLiabilities")
+        market_cap = quote["marketCap"]
+        ncav_ratio = nca / market_cap
+
+        return ncav_ratio
